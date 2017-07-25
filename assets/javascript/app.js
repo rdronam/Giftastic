0d83d6ac2df649c732079f26b3101005d8401e18
unknown <you@example.com>
2017-07-24 19:23:54 -0500

    clean up on javascript text

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
     $('button').on('click', function() {
-        var animal = $(this).data('name');
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var superhero = $(this).data('name');
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + superhero + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         $.ajax({
             url: queryURL,
@@ -17,29 +17,29 @@ $(document).ready(function(){
 
                 for (var i = 0; i < results.length; i++) {
 
-                    var animalDiv = $('<div/>');
+                    var superheroDiv = $('<div/>');
 
                     var p =$('<p/>');
 
                     p.text(results[i].rating);
 
-                    var animalImage = $('<img/>');
+                    var superheroImage = $('<img/>');
 
-                    animalImage.addClass('anImg')
+                    superheroImage.addClass('anImg')
 
-                    animalImage.attr('src', results[i].images.fixed_height.url);
+                    superheroImage.attr('src', results[i].images.fixed_height.url);
 
-                    animalImage.attr('data-still', results[i].images.fixed_height_still.url)
+                    superheroImage.attr('data-still', results[i].images.fixed_height_still.url)
 
-                    animalImage.attr('data-animate', results[i].images.fixed_height.url)
+                    superheroImage.attr('data-animate', results[i].images.fixed_height.url)
 
                     .attr('data-state', 'still');
 
-                    animalDiv.append(p);
+                    superheroDiv.append(p);
 
-                    animalDiv.append(animalImage);
+                    superheroDiv.append(superheroImage);
 
-                    animalDiv.prependTo($('#gifs'));
+                    superheroDiv.prependTo($('#gifs'));
                 }
 
                 $('.anImg').on('click', function() {
@@ -63,23 +63,23 @@ $(document).ready(function(){
             });
     });
 
-    var animals = [''];
+    var superheros = [''];
 
     
         //This function "adds" the buttons 
 
         // handles the event when clicked
         $('#theButton').on('click', function(){
-            var animalButton = $("#gif-input").val();
+            var superheroButton = $("#gif-input").val();
             //adds the new animal
 
-            var newButton = $("<button/>").addClass( "btn btn-info animal").attr('data-name',animalButton).html(animalButton).css({'margin': '5px'});
+            var newButton = $("<button/>").addClass( "btn btn-info superhero").attr('data-name', auperheroButton).html(superheroButton).css({'margin': '5px'});
             
             $("#animalsbuttons").append(newButton);
                 console.log("Work");
 
             queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalButton + "&api_key=dc6zaTOxFJmzC&limit=10";
-                console.log(animalButton);
+                console.log(superheroButton);
 
             $.ajax({
             url: queryURL,
@@ -92,29 +92,29 @@ $(document).ready(function(){
 
                 for (var i = 0; i < results.length; i++) {
 
-                    var animalDiv = $('<div/>');
+                    var superheroDiv = $('<div/>');
 
                     var p =$('<p/>');
 
                     p.text(results[i].rating);
 
-                    var animalImage = $('<img/>');
+                    var superheroImage = $('<img/>');
 
-                    animalImage.addClass('anImg')
+                    superheroImage.addClass('anImg')
 
-                    animalImage.attr('src', results[i].images.fixed_height_still.url);
+                    superheroImage.attr('src', results[i].images.fixed_height_still.url);
 
-                    animalImage.attr('data-still', results[i].images.fixed_height_still.url)
+                    superheroImage.attr('data-still', results[i].images.fixed_height_still.url)
 
-                    animalImage.attr('data-animate', results[i].images.fixed_height.url)
+                    superheroImage.attr('data-animate', results[i].images.fixed_height.url)
 
                     .attr('data-state', 'still');
 
-                    animalDiv.append(p);
+                    superheroDiv.append(p);
 
-                    animalDiv.append(animalImage);
+                    superheroDiv.append(superheroImage);
 
-                    animalDiv.prependTo($('#gifs'));
+                    superheroDiv.prependTo($('#gifs'));
                 }
 
                 $('.anImg').on('click', function() {
